3379d31eb280e88fd11ae10142d87f5a70b9a494
unknown <leejeayoung875@gmail.com>
2024-06-07 12:14:59 +0900

    Login부분 FsReadFile 테스트 완

--- server.js
+++ server.js
@@ -15,7 +15,18 @@ const linksModule = require('./module/fs.js');
 const generateAuthLinks =require('./module/LoginLink.js')
 const fsReadFile =require('./module/fsReadFile.js')
 const { serveCssFile } = require("./module/css.js");
-
+function serveHtmlFile(filePath, res) {
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error(`${filePath} 읽기 오류:`, err);
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Internal Server Error");
+      return;
+    }
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+  });
+}
 
 const server = http.createServer((req, res) => {
   const cookies = parseCookies(req.headers.cookie);
@@ -128,16 +139,7 @@ const server = http.createServer((req, res) => {
     res.end();
   } 
   else if (req.method === "GET" && req.url === "/login") {
-    fs.readFile("login.html", "utf8", (err, data) => {
-      if (err) {
-        console.error("login.html 읽기 오류:", err);
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Internal Server Error");
-        return;
-      }
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    });
+    serveHtmlFile("login.html", res);
   } else if (req.method === "GET" && req.url === "/signup") {
     //get이고 /signup일떄 실행되고 signup.html을 읽는다
     fs.readFile("signup.html", "utf8", (err, data) => {
